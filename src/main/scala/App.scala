4e85f08c3c166b894b8006a164aedf5bf1fb22e2
Muntis Grube <muntis@gmail.com>
2024-10-09 11:06:18 +0300

    Cleanup

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -41,32 +41,5 @@ object App
     App.auditLoginFailure(u, errorText, credentials)
   }
 
-
-  override def validateFields(viewName: String, instance: Map[String, Any])(implicit state: ApplicationState): Unit = {
-    val viewDef = qe.viewDef(viewName)
-    // TODO ensure field ordering
-    val errorMessages = viewDef.fields
-      .filterNot(_.api.readonly)
-      .map(fld =>
-        validationErrorMessage(viewName, fld, instance.getOrElse(fld.fieldName, null))(state.locale))
-      .filter(_.isDefined)
-      .map(_.get)
-      .filter(_ != null)
-    if (errorMessages.nonEmpty)
-      throw new BusinessException(errorMessages.mkString("\n"))
-
-    // TODO merge all errorMessages?
-    val complexFields = viewDef.fields.filter(_.type_.isComplexType).map(fld => fld.fieldName -> fld.type_.name)
-    complexFields.foreach { case (fieldName, typeName) =>
-      instance.getOrElse(fieldName, null) match {
-        case m: Map[String, Any]@unchecked => validateFields(typeName, m)
-        case l: Seq[Map[String, Any]]@unchecked => l.foreach(validateFields(typeName, _))
-        case null =>
-      }
-    }
-  }
-
-
-
 }
 
