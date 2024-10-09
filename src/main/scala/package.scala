eb81130259ced04be4134e16e28b3e2a1f2d551a
Muntis Grube <muntis@gmail.com>
2024-10-09 13:45:29 +0300

    Drop unused methods

--- src/main/scala/package.scala
+++ src/main/scala/package.scala
@@ -12,48 +12,4 @@ package object app {
   def now = new java.sql.Timestamp(currentTime)
   def nowDate = new java.sql.Date(currentTime)
 
-  def mapToJavaMap(map: Map[String, ?]): java.util.Map[String, AnyRef] = {
-    val result = map.map { (entry: (String, ?)) =>
-      (entry._1,
-        entry._2 match {
-          case l: List[_] => listToJavaList(l)
-          case m: Map[String@unchecked, _] => mapToJavaMap(m)
-          case r => r
-        }
-      )
-    }
-    result.asInstanceOf[Map[String, AnyRef]].asJava
-  }
-
-  def listToJavaList(list: List[?]): java.util.List[?] = {
-    val result = list.toList.map {
-      case l: List[_] => listToJavaList(l)
-      case m: Map[String@unchecked, _] => mapToJavaMap(m)
-      case r => r
-    }
-    result.asJava
-  }
-
-  def javaMapToMap(map: java.util.Map[String, ?]): Map[String, ?] = {
-    val result = map.asScala.map(entry =>
-      (entry._1,
-        entry._2 match {
-          case l: java.util.List[_] => javaListToList(l)
-          case m: java.util.Map[String@unchecked, _] => javaMapToMap(m)
-          case r => r
-        }
-      )
-    ).toMap
-    result.asInstanceOf[Map[String, ?]]
-  }
-
-  def javaListToList(list: java.util.List[?]): List[?] = {
-    val result = list.asScala.toList.map {
-      case l: java.util.List[_] => javaListToList(l)
-      case m: java.util.Map[String@unchecked, _] => javaMapToMap(m)
-      case r => r
-    }
-    result
-  }
-
 }
