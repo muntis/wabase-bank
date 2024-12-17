d02572dddc9e68666203be76cab35c50b4624061
Muntis Grube <muntis@gmail.com>
2024-12-17 11:31:27 +0200

    User creation test

--- src/it/scala/BusinessScenariosSpecs.scala
+++ src/it/scala/BusinessScenariosSpecs.scala
@@ -11,8 +11,10 @@ class BusinessScenariosSpecs extends BusinessScenariosBaseSpecs("business") with
   override def deferredResultUri(hash: String) = s"services/deferred/$hash/result"
 
   val RandomDecimalNumberStringPattern = "randomDecimalNumberString\\((\\d*)\\)".r
+  val RandomEmailPattern = "randomEmail"
   def bankFunctions: Map[String, Any] => PartialFunction[String, Any] = context => {
     case RandomDecimalNumberStringPattern(length) => (1 to length.toInt).map(_ => ('0' + Random.nextInt(10)).toChar).mkString
+    case RandomEmailPattern => Random.alphanumeric.take(20).mkString + "@example.com"
   }
 
   override def templateFunctions: Map[String, Any] => PartialFunction[String, Any] =
